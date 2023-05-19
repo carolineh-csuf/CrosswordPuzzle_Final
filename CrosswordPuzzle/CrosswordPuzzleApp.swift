ee6baca7d4b7b0e092ebe81759324aa578595fd9
carolineh-csuf <carolineh@csu.fullerton.edu>
2023-05-18 20:11:43 -0700

    Initial Commit

---
+++ CrosswordPuzzle/CrosswordPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrosswordPuzzleApp.swift
+//  CrosswordPuzzle
+//
+//  Created by csuftitan on 5/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CrosswordPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
